c18ede551fd0ffe13778a312e61e118104fa3d1e
Reynzzz <Rayhanay115@gmail.com>
2025-12-25 02:06:53 +0700

    update api v2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
-const API_BASE_URL = import.meta.env.BASE_URL||'http://localhost:3000/api';
-
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL||'http://localhost:3000/api';
+console.log(API_BASE_URL);
 
 
 // Helper function untuk fetch API
